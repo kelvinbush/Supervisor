6434dae2ccfcef41a692205a7130bd353f616e4c
Kelvin Bush <kelybush@gmail.com>
2021-06-11 11:46:16 +0300

    fixing the theme

--- app/src/main/java/com/kelvinbush/supervisor/ui/create_schedhule/CreateScheduleFragment.kt
+++ app/src/main/java/com/kelvinbush/supervisor/ui/create_schedhule/CreateScheduleFragment.kt
@@ -1,10 +1,13 @@
 package com.kelvinbush.supervisor.ui.create_schedhule
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,16 +16,21 @@ import com.kelvinbush.supervisor.databinding.CreateScheduleFragmentBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kelvinbush.supervisor.R
 import com.kelvinbush.supervisor.adapters.AssignmentsAdapter
-import com.kelvinbush.supervisor.ui.home.HomeViewModel
+import com.kelvinbush.supervisor.domains.Region
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
-class CreateScheduleFragment : Fragment() {
+class CreateScheduleFragment : Fragment(),
+    AdapterView.OnItemSelectedListener {
 
     private var _binding: CreateScheduleFragmentBinding? = null
     private val binding get() = _binding!!
-    private val createScheduleViewModel: CreateScheduleViewModel by viewModels()
+    private val viewModel: CreateScheduleViewModel by viewModels()
+    private var arrayAdapter: ArrayAdapter<String>? = null
+    private val allRegions = arrayListOf<String>()
+    private var assignmentRegions: List<Region>? = null
+    private val assignmentsAdapter = AssignmentsAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,13 +43,30 @@ class CreateScheduleFragment : Fragment() {
 
         val navController = this@CreateScheduleFragment.findNavController()
 
-        val assignmentsAdapter = AssignmentsAdapter()
+//        val appBarConfiguration = AppBarConfiguration(navController.graph)
+//        navController.navigateUp(appBarConfiguration)
+
         val layoutManager = LinearLayoutManager(requireContext())
         layoutManager.orientation = LinearLayoutManager.HORIZONTAL
 
+        binding.routeSpinner.onItemSelectedListener = this@CreateScheduleFragment
 
+        viewModel.regions.observe(viewLifecycleOwner, {
+            if (allRegions.isEmpty()) {
+                it.forEach { region ->
+                    allRegions.add(region.name)
+                }
+            }
+            arrayAdapter = ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_item,
+                allRegions
+            )
+            binding.routeSpinner.adapter = arrayAdapter
+            assignmentRegions = it
+            Log.d("TAG", "onCreateView: $it")
 
-
+        })
 
         return root
     }
@@ -49,5 +74,18 @@ class CreateScheduleFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        getAssignmentsForRegion(position)
+    }
+
+    private fun getAssignmentsForRegion(i: Int) {
+        assignmentsAdapter.submitList(assignmentRegions?.get(i)?.assignments)
+
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/kelvinbush/supervisor/ui/create_schedhule/CreateScheduleViewModel.kt
+++ app/src/main/java/com/kelvinbush/supervisor/ui/create_schedhule/CreateScheduleViewModel.kt
@@ -1,9 +1,34 @@
 package com.kelvinbush.supervisor.ui.create_schedhule
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.kelvinbush.supervisor.domains.Region
+import com.kelvinbush.supervisor.repo.SupervisorRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
+
+private const val TAG = "CreateScheduleViewModel"
 
 @HiltViewModel
-class CreateScheduleViewModel  : ViewModel() {
+class CreateScheduleViewModel @Inject constructor(
+    private val repository: SupervisorRepository
+) : ViewModel() {
+
+    private var _regions = MutableLiveData<List<Region>>()
+
+    val regions:LiveData<List<Region>>
+        get() = _regions
+
+    init {
+        getRegions()
+    }
+
+    private fun getRegions() {
+        Log.d(TAG, "getRegions: called")
+        _regions.value = repository.getRegionsWithAssignments()
+    }
+
 }
